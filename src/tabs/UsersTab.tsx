57d165f41f66cffa1cbc054ddbcf290e423ca622
daavidwlf <wolf_david@gmx.de>
2024-08-30 23:46:41 +0200

    feat: err handling

--- src/tabs/UsersTab.tsx
+++ src/tabs/UsersTab.tsx
@@ -5,6 +5,7 @@ import styles from './UsersTab.module.css'
 import UserListComp from "../components/UserListComp"
 import TextInput from "../components/TextInput"
 import { AxiosError, AxiosResponse } from "axios"
+import LoadingComponent from "../components/LoadingComponent"
 
 type Props = {
     setEditUser: Function
@@ -18,13 +19,19 @@ export default function UsersTab({rerender, setDeleteUser, setEditUser}:Props){
 
     const [search, setSearch] = createSignal<string>("")
 
+    const [loading, setLoading] = createSignal<boolean>(false)
+
+    const [err, setErr] = createSignal<string | null>(null)
+
     createEffect(()=>{
         rerender()
-        API.GET("/users", setUsers, null, null)
+        API.GET("/users", setUsers, null, setLoading)
     })
 
     const enterSearch = (event?: SubmitEvent) =>{
         event?.preventDefault()
+
+        setErr(null)
 
         if(search() !== ""){
             API.POST("/user/search", 
@@ -34,21 +41,23 @@ export default function UsersTab({rerender, setDeleteUser, setEditUser}:Props){
                     "email": search(),
                     "userId": search()
                 },
-                (response: AxiosResponse) => {
+                (response: AxiosResponse) => { 
+                    setErr(null)
                     setUsers(response.data)
                 },
                 (err: AxiosError) => {
-                    console.log(err.response?.data?.message)
+                    // @ts-ignore
+                    setErr(err.response?.data?.message ? err.response?.data?.message :err.message )
                 },
-                null
+                setLoading
             )
         } 
     }
 
     createEffect(()=>{
         if(search() === ""){
-            console.log("fetch")
-            API.GET("/users", setUsers, null, null)
+            setErr(null)
+            API.GET("/users", setUsers, null, setLoading)
         }  
     })
 
@@ -78,20 +87,35 @@ export default function UsersTab({rerender, setDeleteUser, setEditUser}:Props){
                         />
                     </span>
                 </div>
-                <div class={styles.list}>
-                    {
-                        users()?.map((item:User, index:number)=>{
-                            return(
-                                <UserListComp
-                                    item={item}
-                                    index={index}
-                                    setEditUser={setEditUser}
-                                    setDeleteUser={setDeleteUser}
-                                />
-                            )
-                        })
+                {
+                loading() ?
+                    <LoadingComponent/>
+                :
+                    err() ? 
+                        <span class={styles.err}>
+                            <h4>{err()}</h4>
+                        </span>
+                    :
+                        <div class={styles.list}>
+                        { 
+                                !users() ?
+                                    <span class={styles.noResults}>
+                                        <h4>Keine Ergebnisse...</h4>
+                                    </span>
+                                :
+                                    users()?.map((item:User, index:number)=>{
+                                        return(
+                                            <UserListComp
+                                                item={item}
+                                                index={index}
+                                                setEditUser={setEditUser}
+                                                setDeleteUser={setDeleteUser}
+                                            />
+                                        )
+                                    })
+                            }
+                        </div>
                     }
-                </div>
             </div>
         </form>
     )
