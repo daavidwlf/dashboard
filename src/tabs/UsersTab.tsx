213260e42c492c3ef12ef2b4c67f95367e84fe99
daavidwlf <wolf_david@gmx.de>
2024-08-30 23:01:56 +0200

    feat, feat, fix: add press enter to search, refetch all when search bar is an empty string, post request userID

--- src/tabs/UsersTab.tsx
+++ src/tabs/UsersTab.tsx
@@ -3,6 +3,8 @@ import API from "../data/API"
 import { User } from "../data/Types"
 import styles from './UsersTab.module.css'
 import UserListComp from "../components/UserListComp"
+import TextInput from "../components/TextInput"
+import { AxiosError, AxiosResponse } from "axios"
 
 type Props = {
     setEditUser: Function
@@ -14,32 +16,78 @@ export default function UsersTab({rerender, setDeleteUser, setEditUser}:Props){
 
     const [users, setUsers] = createSignal<User[]>()
 
+    const [search, setSearch] = createSignal<string>("")
+
     createEffect(()=>{
         rerender()
         API.GET("/users", setUsers, null, null)
     })
 
-    return(
-        <div class={styles.container}>
-            <div class={styles.top}>
-                <h2>Users</h2>
-                <span class={styles.topRight}>
-                </span>
-            </div>
-            <div class={styles.list}>
+    const enterSearch = (event?: SubmitEvent) =>{
+        event?.preventDefault()
+
+        if(search() !== ""){
+            API.POST("/user/search", 
                 {
-                    users()?.map((item:User, index:number)=>{
-                        return(
-                            <UserListComp
-                                item={item}
-                                index={index}
-                                setEditUser={setEditUser}
-                                setDeleteUser={setDeleteUser}
-                            />
-                        )
-                    })
-                }
+                    "firstName": search(),
+                    "lastName": search(),
+                    "email": search(),
+                    "userId": search()
+                },
+                (response: AxiosResponse) => {
+                    setUsers(response.data)
+                },
+                (err: AxiosError) => {
+                    console.log(err.response?.data.message)
+                },
+                null
+            )
+        } 
+    }
+
+    createEffect(()=>{
+        if(search() === ""){
+            console.log("fetch")
+            API.GET("/users", setUsers, null, null)
+        }  
+    })
+
+    return(
+        <form onsubmit={enterSearch}>
+            <div class={styles.container}>
+                <div class={styles.top}>
+                    <h2>Users</h2>
+                    <span class={styles.topRight}>
+                        <TextInput
+                            icon={<i class="fa-light fa-magnifying-glass"></i>}
+                            type="text"
+                            placeholder="User Suchen"
+                            value={search}
+                            callback={setSearch}
+                        />
+                        <button
+                            class={styles.enterSearch}
+                            type="submit"
+                        >
+                            <i class="fa-light fa-turn-down-left"></i>
+                        </button>
+                    </span>
+                </div>
+                <div class={styles.list}>
+                    {
+                        users()?.map((item:User, index:number)=>{
+                            return(
+                                <UserListComp
+                                    item={item}
+                                    index={index}
+                                    setEditUser={setEditUser}
+                                    setDeleteUser={setDeleteUser}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </div>
-        </div>
+        </form>
     )
 }
